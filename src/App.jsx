4c2c4b1440ad0c204eac26fc01cb224ab1928987
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-01-26 11:24:02 +0600

    Navbar end

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './sections/navbar/Navbar'
+import Background from '../src/assets/images/background.svg'
 
 const App = () => {
   return (
     <>
-      <Navbar/>
+    <section>
+        <div className="container" style={{backgroundImage: `url(${Background})`}}>
+            <Navbar/>
+        </div>
+    </section>
     </>
   )
 }
--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import './navbar.css'
+import Logo from '../../assets/images/logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <section id='navbar'>
+        <div className="navbar">
+            <div className="nav_wrapper">
+                <div className="nav_logo">
+                    <a href="#" className='logo_image'>
+                        <img src={Logo} alt="LogoImage" />
+                    </a>
+                    <h2>Nuruddin</h2>
+                </div>
+                <div className="nav_menu">
+                    <ul className='nav_list'>
+                        <li className='nav_item'>
+                            <a href="#" >profile</a>
+                        </li>
+                        <li className='nav_item'>
+                            <a href="#" >projects</a>
+                        </li>
+                        <li className='nav_item'>
+                            <a href="#" >contact</a>
+                        </li>
+                        <li className='nav_item'>
+                            <a href="#" >about</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 

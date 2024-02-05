1b83d6dcc8dd6e9219e12ac308604503639e7888
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-02-05 22:17:15 +0600

    Navbar Toggle Edit

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,27 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import './navbar.css'
 import Logo from '../../assets/images/logo.png'
 import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
-    
-    // useEffect(()=>{
-    //     let navList = document.querySelector('.nav_list')
-    //     let menuBar = document.querySelector('#menu_bar')
-    //     function show() {
-    //         navList.classList.toggle("show")
-    //     };
-    //     return(()=>{
-    //         menuBar.addEventListener("click", ()=>{
-    //             show()
-    //         })
-    //     })
-    // },[])
-    let menuOpenn = ()=>{
-        let navList = document.querySelector('.nav_list')
-        navList.classList.toggle("show");
+    let navMenu = document.querySelector('#navMenu');
+    let navRrf = useRef(null)
+    let handleClick = ()=>{
+        if (navRrf.current.style.opacity == "0") {
+            navRrf.current.style.opacity = "1"
+        }else{
+            navRrf.current.style.opacity = "0"
+        }
     }
-
+       
   return (
     <section id='navbar'>
         <div className="navbar">
@@ -32,7 +24,7 @@ const Navbar = () => {
                     </a>
                 </div>
                 <div className="nav_menu">
-                    <ul className='nav_list'>
+                    <ul className='nav_list' id='navMenu' ref={navRrf}>
                         <li className='nav_item'>
                             <a href="#profile" >profile</a>
                         </li>
@@ -53,8 +45,9 @@ const Navbar = () => {
                         </li>
                     </ul>
                 </div>
+                <div  className="hello"></div>
                 <div className="bars">
-                <FaBars onClick={menuOpenn}  className='menu_bar' id='menu_bar' />
+                <FaBars onClick={handleClick} id='bar'/>
                 </div>
             </div>
         </div>

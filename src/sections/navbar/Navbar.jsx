8f41136459acf87c440b5e25f7a95defd66bad53
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-02-06 17:24:31 +0600

    Nav responsive

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './navbar.css'
 import Logo from '../../assets/images/logo.png'
 import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
-    let navMenu = document.querySelector('#navMenu');
-    let navRrf = useRef(null)
-    let handleClick = ()=>{
-        if (navRrf.current.style.opacity == "0") {
-            navRrf.current.style.opacity = "1"
-        }else{
-            navRrf.current.style.opacity = "0"
-        }
-    }
-       
+  let [navMenu, setNavMenu] = useState(false)
+  useEffect(()=>{
+    let navList = document.querySelector('.nav_list')
+    navList.style.maxHeight = "0px"
+    let navBar = document.querySelector('#navbar')
+    navBar.style.paddingBottom = "0px"
+  },[])
+  let handleClick = ()=>{
+    setNavMenu(!navMenu)
+  }
   return (
-    <section id='navbar'>
+    <section id='navbar' style={{paddingBottom: navMenu ? '390px' : '0px'}}>
         <div className="navbar">
             <div className="nav_wrapper">
                 <div className="nav_logo">
@@ -24,7 +24,7 @@ const Navbar = () => {
                     </a>
                 </div>
                 <div className="nav_menu">
-                    <ul className='nav_list' id='navMenu' ref={navRrf}>
+                    <ul className='nav_list' style={{ maxHeight: navMenu ? '376px' : '0px' }}>
                         <li className='nav_item'>
                             <a href="#profile" >profile</a>
                         </li>
@@ -45,9 +45,8 @@ const Navbar = () => {
                         </li>
                     </ul>
                 </div>
-                <div  className="hello"></div>
-                <div className="bars">
-                <FaBars onClick={handleClick} id='bar'/>
+                <div onClick={handleClick} className="bars">
+                  <FaBars  id='bar'/>
                 </div>
             </div>
         </div>

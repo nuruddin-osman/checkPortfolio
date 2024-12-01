793b22a7730816b3f51844f36e6c3afd59844949
mnosumon <mdnuruddin378350@gmail.com>
2024-12-01 14:22:36 +0600

    simple design custoize

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,43 +1,50 @@
-import React from 'react'
-import './about.css'
-import { FaFacebookF} from "react-icons/fa";
+import React from "react";
+import "./about.css";
+import { FaFacebookF } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa6";
-import AboutImage from '../../assets/images/aboutImage.png'
+import AboutImage from "../../assets/images/aboutImage.png";
 import { FiGithub, FiTwitter } from "react-icons/fi";
 
 const About = () => {
   return (
-    <section id='about'>
-        <div className="about">
-          <h2 className='section_heading'>About me</h2>
-          <div className="about_wrapper">
-            <div className="about_media">
-              <div className="about_image">
-                <img src={AboutImage} alt="About Image" />
-                <div className="about_social">
-                  <a href="https://www.facebook.com/mnos365/">
-                    <FaFacebookF className='about_icon facebook'/>
-                  </a>
-                  <a href="https://twitter.com/NuruddinDev">
-                    <FiTwitter className='about_icon twitter'/>
-                 </a>
-                  <a href="https://github.com/mnosumon">
-                    <FiGithub className='about_icon github' />
-                  </a>
-                  <a href="https://www.linkedin.com/in/md-nuruddin-osman-b78334209/">
-                    <FaLinkedinIn className='about_icon linkedin'/>
-                  </a>
-                </div>
+    <section id="about">
+      <div className="about">
+        <h2 className="section_heading">About me</h2>
+        <div className="about_wrapper">
+          <div className="about_media">
+            <div className="about_image">
+              <img src={AboutImage} alt="About Image" />
+              <div className="about_social">
+                <a href="https://www.facebook.com/mnos365/">
+                  <FaFacebookF className="about_icon facebook" />
+                </a>
+                <a href="https://twitter.com/NuruddinDev">
+                  <FiTwitter className="about_icon twitter" />
+                </a>
+                <a href="https://github.com/mnosumon">
+                  <FiGithub className="about_icon github" />
+                </a>
+                <a href="https://www.linkedin.com/in/md-nuruddin-osman-b78334209/">
+                  <FaLinkedinIn className="about_icon linkedin" />
+                </a>
               </div>
             </div>
-            <div className="about_content">
-              <article>I'm a junior front-end developer and I love to code whenever I can. I have a clear understanding of this Frameworks and Libraries, Responsive Web Design, CSS and Styling, JavaScript, Cross-Browser Compatibility, Accessibility. Currently I am working with well known frontend framework like React. Using HTML, CSS, and JavaScript, I have expertise in creating responsive websites</article>
-              <a href="#contact">contact me</a>
-            </div>
+          </div>
+          <div className="about_content">
+            <article>
+              I'm a junior front-end developer and I love to code whenever I
+              can. I have a clear understanding of this Frameworks and
+              Libraries, Responsive Web Design, CSS and Styling, JavaScript,
+              Cross-Browser Compatibility, Accessibility. Currently I am working
+              with well known frontend framework like React. Using HTML, CSS,
+              and JavaScript, I have expertise in creating responsive websites
+            </article>
+            <a href="#contact">contact me</a>
           </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/sections/banner/Banner.jsx
+++ src/sections/banner/Banner.jsx
@@ -1,37 +1,45 @@
-import React from 'react'
-import './banner.css'
-import BannerImage from '../../assets/images/banner.jpg'
-import pdfDownload from '../../assets/images/Nuruddin-Front-end-Developer.pdf'
+import React from "react";
+import "./banner.css";
+import BannerImage from "../../assets/images/banner.jpg";
+import pdfDownload from "../../assets/images/Nuruddin-Front-end-Developer.pdf";
 
 const Banner = () => {
   return (
-    <section id='profile'>
-        <div className="profile">
-            <div className="prifile_content">
-                <div className="profile_heading">
-                  <span>hi, I'm </span>
-                  <h4>md nuruddin osman,</h4>
-                </div>
-                <div className="profile_title">
-                  <h3>i'm a junior web developer</h3>
-                </div>
-                <div className="profile_description">
-                  <p>To establish myself as a Front-end Developer in different local, and international by offering the value/benefit of Brand, Visualization, and Promotion. Eager to contribute creativity and technical skills to dynamic projects through responsive and visually appealing web interfaces. Want to establish myself as a Sr. Web Developer within 4-5 years.
-                  </p>
-                  <a href='#project' >View my projects</a>
-                </div>
-            </div>
-            <div className="profile_pictue">
-                <div className="profile_images">
-                  <img src={BannerImage} alt="picture" />
-                </div>
-            </div>
+    <section id="profile">
+      <div className="profile">
+        <div className="prifile_content">
+          <div className="profile_heading">
+            <span>hi, I'm </span>
+            <h4>md nuruddin osman,</h4>
+          </div>
+          <div className="profile_title">
+            <h3>i'm a junior web developer</h3>
+          </div>
+          <div className="profile_description">
+            <p>
+              To establish myself as a Front-end Developer in different local,
+              and international by offering the value/benefit of Brand,
+              Visualization, and Promotion. Eager to contribute creativity and
+              technical skills to dynamic projects through responsive and
+              visually appealing web interfaces. Want to establish myself as a
+              Sr. Web Developer within 4-5 years.
+            </p>
+            <a href="#project">View my projects</a>
+          </div>
         </div>
-        <div className="profile_cv">
-          <a href={pdfDownload} download>Download CV</a>
+        <div className="profile_pictue">
+          <div className="profile_images">
+            <img src={BannerImage} alt="picture" />
+          </div>
         </div>
+      </div>
+      <div className="profile_cv">
+        <a href={pdfDownload} download>
+          Download CV
+        </a>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
